685dfe624ec36de3adb389f4ac8b1ac73ec5a206
brrock <123623302+brrock@users.noreply.github.com>
2024-06-10 07:28:44 +0100

    fix

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,7 +5,6 @@ import { revalidatePath } from "next/cache";
 import User from "../database/models/user.model";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
-
 // CREATE
 export async function createUser(user: CreateUserParams) {
   try {
