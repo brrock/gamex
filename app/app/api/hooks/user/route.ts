ad9b6a6d55dca230e5041a366deb40e332c3f7fa
brrock <123623302+brrock@users.noreply.github.com>
2024-07-12 09:06:49 +0100

    prisma

---
+++ app/app/api/hooks/user/route.ts
@@ -0,0 +1,79 @@
+import type { WebhookEvent } from '@clerk/clerk-sdk-node';
+import { Webhook } from 'svix';
+import { prisma } from '@/lib/prisma';
+
+export async function handleUserWebhook(request: Request): Promise<Response> {
+  try {
+    const { type, data } = await request.json() as WebhookEvent;
+    const headers = {
+      'svix-id': request.headers.get('SVIX-ID') as string,
+      'svix-timestamp': request.headers.get('SVIX-TIMESTAMP') as string,
+      'svix-signature': request.headers.get('SVIX-SIGNATURE') as string,
+    };
+
+    if (!headers['svix-id'] || !headers['svix-timestamp'] || !headers['svix-signature']) {
+      return new Response('Missing headers', { status: 400 });
+    }
+
+    const signingSecret = process.env.SVIX_USER_HOOK_SIGNING_SECRET || 'defaultSecret';
+    const webhook = new Webhook(signingSecret);
+    
+    try {
+      webhook.verify(JSON.stringify(data), headers);
+    } catch (error) {
+      return new Response('Invalid signature', { status: 400 });
+    }
+
+    switch (type) {
+      case 'user.created':
+        await createUser(data);
+        return new Response('User created');
+      case 'user.updated':
+        await updateUser(data);
+        return new Response('User updated');
+      case 'user.deleted':
+        if (data.id === undefined) {
+          return new Response('User deleted with undefined id', { status: 400 });
+        }
+        await deleteUser(data.id);
+        return new Response('User deleted');
+      default:
+        return new Response('Unhandled webhook event', { status: 400 });
+    }
+  } catch (error) {
+    return new Response('Invalid request body', { status: 400 });
+  }
+}
+
+async function createUser(data: WebhookEvent['data']) {
+  await prisma.user.upsert({
+    where: { id: data.id },
+    create: {
+      id: data.id,
+      username: data.username,
+      imageUrl: data.profile_image_url,
+    },
+    update: {
+      username: data.username,
+      imageUrl: data.profile_image_url,
+    },
+  });
+}
+
+async function updateUser(data: WebhookEvent['data']) {
+  await prisma.user.update({
+    where: { id: data.id },
+    data: {
+      username: data.username,
+      imageUrl: data.profile_image_url,
+    },
+  });
+}
+
+async function deleteUser(userId: string) {
+  await prisma.user.delete({
+    where: {
+      id: userId,
+    },
+  });
+}
