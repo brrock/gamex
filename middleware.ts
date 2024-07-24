5d42139d3258ef662b2f0e867de2a8c58c9684dc
brrock <123623302+brrock@users.noreply.github.com>
2024-07-24 09:09:07 +0100

    Refactor and remove unused depencies

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-// Make sure that the `/api/webhooks/(.*)` route is not protected here
-export default clerkMiddleware()
+export default clerkMiddleware((auth) => {
+  auth().protect();
+});
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
